613a95f0b08328b7fa7721849bd32cbac845a9c6
MOMOJMOGG <momomacaca@gmail.com>
2021-07-12 20:05:29 +0800

    tmp saving for testing others route

--- controller/apiController.js
+++ controller/apiController.js
@@ -4,6 +4,8 @@ const Tweet = db.Tweet
 const Like = db.Like
 const Followship = db.Followship
 const Reply = db.Reply
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const apiController = {
   getTweet: async (req, res) => {
@@ -15,6 +17,45 @@ const apiController = {
         ]
       })
       return res.json(tweet.toJSON())
+    } catch (err) {
+      return console.warn(err)
+    }
+  },
+
+  getUser: async (req, res) => {
+    try {
+      const { userId } = req.params
+      const user = await User.findByPk(userId, { attributes: ['id', 'cover', 'avatar', 'name', 'introduction'] })
+      return res.json(user.toJSON())
+    } catch (err) {
+      return console.warn(err)
+    }
+  },
+
+  postUser: async (req, res) => {
+    try {
+      const { userId } = req.params
+      const { name, introduction, avatar, cover } = req.body
+      if (!name) {
+        //req.flash('error_message', 'errors')
+        return res.redirect(`/users/${userId}/tweets`)
+      }
+      const { file } = req
+      if (file) {
+        imgur.setClientID(IMGUR_CLIENT_ID);
+        imgur.upload(file.path, async (err, img) => {
+          const user = await User.findByPk(userId)
+          await user.update({
+            name: name,
+            introduction: introduction,
+            avatar: avatar,
+            cover: cover,
+            image: file ? img.data.link : restaurant.image,
+          })
+          return callback({ status: 'success', message: 'restaurant was successfully updated' })
+        })
+      }
+
     } catch (err) {
       return console.warn(err)
     }
