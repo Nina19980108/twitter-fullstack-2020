ca9e7a239314a7e8fe57024744a7aa30d68489c4
afwyou <afwyousenge@gmail.com>
2021-07-17 17:56:46 +0800

    modify publicSocketPage so that could show the typing area to send message

--- controller/socketController.js
+++ controller/socketController.js
@@ -1,6 +1,10 @@
 const socketController = {
   getPublicSocket: (req, res) => {
-    return res.render('publicSocket')
+    const publicSocketPage = true
+
+    return res.render('publicSocket', {
+      publicSocketPage
+    })
   }
 }
 module.exports = socketController 
